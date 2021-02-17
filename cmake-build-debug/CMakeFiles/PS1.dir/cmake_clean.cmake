9f538c9998e3576524bcd19f2eb7349e85ae5ac5
Nikki001021 <nikkili@bu.edu>
2021-02-17 14:45:02 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PS1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PS1.dir/main.cpp.o"
+  "PS1.pdb"
+  "PS1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,39 +1,6 @@
-#include <stdio.h>
-#include "maths.h"
+#include <iostream>
 
 int main() {
-    float Number1, Number2;
-    char Operator;
-    int choice;
-    do {
-        printf("Enter a mathematical expression using the following operators (+, -, *, /, ^, $): ");
-        scanf("%f%c%f", &Number1, &Operator, &Number2);
-        switch(Operator) {
-            case '+':
-                printf("Result: <%.2f>\n", add(Number1, Number2));
-                break;
-            case '-':
-                printf("Result: <%.2f>\n", subtract(Number1, Number2));
-                break;
-            case '*':
-                printf("Result: <%.2f>\n", multiply(Number1, Number2));
-                break;
-            case '/':
-                printf("Result: <%.2f>\n", divide(Number1, Number2));
-                break;
-            case '^':
-                printf("Result: <%.2f>\n", power(Number1, Number2));
-                break;
-            case '$':
-                printf("Result: <%.2f>\n", recursivePower(Number1, Number2));
-                break;
-            default:
-                printf("Wrong Operator!\n");
-                break;
-        }
-        printf("Would you like to enter another expression? (Yes = 1; No = 0): ");
-        scanf("%d", &choice);
-    } while (choice != 0);
-    printf("Bye!");
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
